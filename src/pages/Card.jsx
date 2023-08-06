e6d2c740ffea7620bf6a528754a9cd633bbac04d
Yusuf <yusuf2002kh@gmail.com>
2023-08-06 08:26:02 +0500

    end?

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -49,8 +49,8 @@ const Card = () => {
 
   return (
     <div className="container mx-auto mt-10">
-      <div className="flex flex-wrap shadow-md my-10">
-        <div className="w-3/4 bg-white px-10 py-10">
+      <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row  shadow-md my-10">
+        <div className="w-3/4 md:w-1/2 w-5/6 bg-white px-10 py-10">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
             <h2 className="font-semibold text-2xl">{product?.length} Items</h2>
@@ -135,7 +135,7 @@ const Card = () => {
           </Link>
         </div>
 
-        <div id="summary" className="w-1/4 px-8 py-10">
+        <div id="summary" className="w-1/4 px-8 py-10 md:w-1/2 w-5/6">
           <h1 className="font-semibold text-2xl border-b pb-8">
             Order Summary
           </h1>
